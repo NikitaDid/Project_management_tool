ade4f3f7010ccbd495f12515f57fee6055c94af7
Nikita <nikitadidenko222@gmail.com>
2025-10-12 14:06:12 +0200

    user model set up and small admin interface

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['username','email','role','team','is_staff']
+    fieldsets = UserAdmin.fieldsets + (  #fieldsets controls the form when editing/creating a user in the admin.
+        (None, {'fields': ('role','team')}),  #('Section Title', {'fields': (list_of_fields)})
+    )
+
+
